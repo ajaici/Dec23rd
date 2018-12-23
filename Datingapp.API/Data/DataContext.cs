be5a070f76a0dfd5febc69ac1aebfd8e25f8b540
Ajay Chacko Idicula <ajay.idicula@xe04.ey.com>
2018-12-23 22:33:42 +0530

    initial commit

---
+++ Datingapp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Datingapp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datingapp.API.Data
+{
+    public class DataContext  : DbContext   
+    {
+        public DataContext(DbContextOptions<DataContext> DbContextOptions) : base(DbContextOptions)
+        {
+            
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
